f18469dad4e772aeb831fa905856885e046aed87
acsaeng <acsaeng@gmail.com>
2021-12-06 11:45:15 -0700

    VET-35 - Connected weight history backend to database

---
+++ Back End/vetapp2/src/main/java/group825/vetapp2/weighthistory/WeightHistoryController.java
@@ -0,0 +1,110 @@
+package group825.vetapp2.weighthistory;
+
+import group825.vetapp2.exceptions.ApiRequestException;
+import group825.vetapp2.exceptions.InvalidIdException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Controller that handles WeightHistoryRequests requests
+ *
+ * @author Aron Saengchan
+ * @version 1.0
+ * @since November 15, 2021
+ */
+@RestController
+@RequestMapping(path = "/app/animal/weight-history")
+public class WeightHistoryController {
+
+    /**
+     * Weight history service that performs the request
+     */
+    private final WeightHistoryService weightHistoryService;
+
+    /**
+     * Constructor that initializes the weightController
+     * @param weightService service that performs the request
+     */
+    @Autowired
+    public WeightHistoryController(WeightHistoryService weightService) {
+        this.weightHistoryService = weightService;
+    }
+
+    /**
+     * 'GET' request that retrieves all weight histories from the database
+     * @return a list of weight histories for all animals
+     */
+    @GetMapping
+    public List<Weight> selectWeight() {
+        return this.weightHistoryService.selectWeightHistory();
+    }
+
+    /**
+     * 'POST' request that adds a weight entry to the database
+     * @param weight weight entry to be added
+     */
+    @PostMapping
+    public void addWeight(@RequestBody Weight weight) {
+        // Checks if any user fields are 'null'
+        if (weight.anyNulls()) {
+            throw new ApiRequestException("Fields cannot be null");
+        }
+
+        this.weightHistoryService.addWeight(weight);
+    }
+
+    /**
+     * 'PUT' request that updates weight history information
+     * @param strId animal's ID number
+     */
+    @PutMapping(path = "/{id}")
+    public void editWeight(@PathVariable("id") String strId) {
+        // User inputs this information
+        LocalDate newDate = LocalDate.of(2021, 1, 1);
+        double newWeight = 10.0;
+
+        UUID id;
+
+        // Check if ID is a valid UUID
+        try {
+            id = UUID.fromString(strId);
+            weightHistoryService.editWeight(id, newDate, newWeight);
+        } catch(java.lang.IllegalArgumentException e) {
+            throw new InvalidIdException();
+        }
+
+        // Check if ID exists
+        if (this.weightHistoryService.editWeight(id, newDate, newWeight) == 0) {
+            throw new InvalidIdException();
+        }
+    }
+
+    /**
+     * 'DELETE' request that deletes a weight entry
+     * @param strId animal's ID number
+     */
+    @DeleteMapping(path = "/{id}")
+    public void deleteWeight(@PathVariable("id") String strId) {
+        // User inputs this information
+        UUID id;
+        LocalDate date = LocalDate.of(2021, 1, 1);
+        int status;
+
+        // Check if ID is a valid UUID
+        try {
+            id = UUID.fromString(strId);
+            status = weightHistoryService.deleteWeight(id, date);
+        } catch(java.lang.IllegalArgumentException e) {
+            throw new InvalidIdException();
+        }
+
+        // Check if ID exists
+        if (status == 0) {
+            throw new InvalidIdException();
+        }
+    }
+}
---
+++ Back End/vetapp2/src/main/java/group825/vetapp2/weighthistory/WeightHistoryRepository.java
@@ -0,0 +1,102 @@
+package group825.vetapp2.weighthistory;
+
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Repository that stores WeightHistory information
+ *
+ * @author Aron Saengchan
+ * @version 1.0
+ * @since November 15, 2021
+ */
+@Repository("tempWeightHistoryRepo")
+public class WeightHistoryRepository {
+
+    /**
+     * Database that stores all the weight histories
+     */
+    private static final List<Weight> database = new ArrayList<>();
+
+    /**
+     * Retrieves all weight histories from the database
+     * @return a list of weight entries of all animals
+     */
+    public List<Weight> selectWeightHistory() {
+        return database;
+    }
+
+    /**
+     * Adds a weight entry to the database
+     * @param weight animal's weight entry
+     * @return 1 if addition was successful, 0 otherwise
+     */
+    public int addWeight(Weight weight) {
+        database.add(weight);
+        return 1;
+    }
+
+    /**
+     * Updates an animal's weight entry
+     * @param id animal's ID number
+     * @param date date that weight was logged
+     * @param weight animal's weight
+     * @return 1 if the update was successful, 0 otherwise
+     */
+    public int editWeight(UUID id, LocalDate date, double weight) {
+        return selectWeightById(id).map(user -> {
+            int accountIdx = database.indexOf(user);
+
+            if (accountIdx >= 0) {
+                database.get(accountIdx).setDate(date);
+                database.get(accountIdx).setWeight(weight);
+                return 1;
+            }
+
+            return 0;
+        }).orElse(0);
+    }
+
+    /**
+     * Searches for an animal's weight entry by ID number in the database
+     * @param id animal's ID number
+     * @return weight entry if found, null otherwise
+     */
+    private Optional<Weight> selectWeightById(UUID id) {
+        return database.stream().filter(weightHistory -> weightHistory.getId().equals(id)).findFirst();
+    }
+
+    /**
+     * Deletes an animal's weight entry
+     * @param id animal's ID number
+     * @param date date that weight was logged
+     * @return 1 if the deletion was successful, 0 otherwise
+     */
+    public int deleteWeight(UUID id, LocalDate date) {
+        return selectWeightByIdAndDate(id, date).map(user -> {
+            int accountIdx = database.indexOf(user);
+
+            if (accountIdx >= 0) {
+                database.remove(accountIdx);
+                return 1;
+            }
+
+            return 0;
+        }).orElse(0);
+    }
+
+    /**
+     * Searches for an animal's weight entry by ID number and date in the database
+     * @param id animal's ID number
+     * @param date date that weight was logged
+     * @return weight entry if found, null otherwise
+     */
+    private Optional<Weight> selectWeightByIdAndDate(UUID id, LocalDate date) {
+        return database.stream().filter(weightHistory -> weightHistory.getId().equals(id) && weightHistory.getDate().equals(date)).findFirst();
+    }
+}
